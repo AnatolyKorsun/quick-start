94373599621623380b6d6db0ac651e9428d24f21
AnatolyKorsun <korsoon@i.ua>
2020-08-04 21:32:13 +0300

    add isotope filter

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,21 +1,24 @@
 // filter menu highlight
-(function(){
-const controls = document.querySelectorAll(".filter__link");
-const activeClass = "filter__item--active";
-controls.forEach(function(control){
-    control.addEventListener("click", function(e){
-        e.preventDefault();
-        controls.forEach(function(link){
-            link.closest(".filter__item").classList.remove(activeClass);
-            //find parent for link with class ...
-        })
-        control.closest('.filter__item').classList.add(activeClass);
-    } )
-}) 
+(function () {
+  const elem = document.querySelector('.product__list');
+  const iso = new Isotope(elem, {
+    itemSelector: '.product__item',
+  });
 
-    // // filter
-    const elem = document.querySelector(".product__list");
-    const iso = new Isotope( elem, {
-      itemSelector: ".product__item",
+  const controls = document.querySelectorAll('.filter__link');
+  const activeClass = 'filter__item--active';
+  controls.forEach(function (control) {
+    control.addEventListener('click', function (e) {
+      e.preventDefault();
+      let filterName = control.getAttribute('data-filter');
+      controls.forEach(function (link) {
+        link.closest('.filter__item').classList.remove(activeClass);
+        //find parent for link with class ...
+      });
+      control.closest('.filter__item').classList.add(activeClass);
+      iso.arrange({
+          filter: `.${filterName}`
+      })
     });
-}())
+  });
+})();
